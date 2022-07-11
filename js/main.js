3f23ecc3a5a216a3a647222976041a3264d7ab1c
yoshi1203 <yoshivn1203@gmail.com>
2022-07-11 21:36:29 +0700

    finish

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function inSoNguyenTo() {
+  let number = document.getElementById('inputNumber').value,
+    ketQua = '';
+  for (i = 1; i <= number; i++) {
+    checkPrime(i) ? (ketQua += ' ' + i) : ketQua;
+  }
+  document.getElementById('ketQua').innerHTML = ketQua;
+}
+
+checkPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+    if (num % i == 0) return false;
+  return num > 1;
+};
